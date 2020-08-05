a67858da1e8bc1e219f39844777bfc293c307e63
Samuel Diethelm <samueldiethelm@gmail.com>
2020-08-05 10:30:42 +0200

    init

---
+++ readme.txt
@@ -0,0 +1,35 @@
+=== Plugin Name ===
+Contributors: samueldiethelm
+Tags: remote thumbnail, external thumbnail, thumbnail, remote featured image, div, background, featured image
+Requires at least: 3.1.0
+Tested up to: 5.4
+License: GPLv3
+License URI: http://www.gnu.org/licenses/gpl-3.0.html
+
+Lightweight plugin to use remote images for post thumbnails and featured image. Enter remote image url into custom field 'remote_thumbnail' of any post.
+
+== Description ==
+
+Lightweight plugin to use remote images for post thumbnails and featured image. Enter remote image url into custom field 'remote_thumbnail' of any post.
+
+Images will not be downloaded, therefore will not use up space from your hosting. In order to keep the same aspect ratio as an uploaded, cropped image with no distortion, the code generated will be an inline-block DIV with the image set as a background.
+
+== Installation ==
+
+1. Upload the plugin files to the `/wp-content/plugins/` directory, or install the plugin through the WordPress plugins screen directly.
+1. Activate the plugin through the 'Plugins' screen in WordPress
+
+== Frequently Asked Questions ==
+
+= Where can I use the logic? =
+
+The plugin can be used in any post type that admits custom fields or postmeta.
+
+= Will images be downloaded to my server? =
+
+No. They will be remotely referenced.
+
+== Changelog ==
+
+= 1.0 =
+* Initial upload
---
+++ remote-thumbnail.php
@@ -0,0 +1,56 @@
+<?php
+/*
+Plugin Name: Remote Thumbnail
+Plugin URI: http://wordpress.org/plugins/remote-thumbnail
+Description: Lightweight plugin to use remote images for post thumbnails and featured image. Enter remote image url into custom field 'remote_thumbnail' of any post.
+Version: 1.0
+Author: Samuel Diethelm
+Author URI: http://profiles.wordpress.org/samueldiethelm
+License: GPLv3
+License URI: http://www.gnu.org/licenses/gpl-3.0.html
+*/
+
+add_filter('post_thumbnail_html', 'remote_post_thumbnail_html', 11, 5);
+function remote_post_thumbnail_html($html, $post_ID, $post_image_id, $size, $attr){
+	$src = get_post_meta($post_ID,'remote_thumbnail',true);
+	
+	if(!$src)
+		return $html;
+		
+	global $_wp_additional_image_sizes;
+	
+	$w = get_option($size.'_size_w',0);
+	$h = get_option($size.'_size_h',0);
+	$c = (bool)get_option($size.'_crop',0);
+	
+	if(isset($_wp_additional_image_sizes[$size]))
+		$size = $_wp_additional_image_sizes[$size];
+	else
+		$size = array('width'=>$w,'height'=>$h,'crop'=>$c);
+	
+	$attr['style'] = sprintf('width:%s ; height:%s ; background-image:url(%s); background-size:cover; background-position:center; display:inline-block;',	$size['width'].'px',$size['height'].'px',$src);
+	
+	$html = '<div';
+	
+	foreach ( $attr as $name => $value ) {
+		$html .= " $name=" . '"' . $value . '"';
+	}
+	
+	$html .= '></div>';
+	
+	return $html;
+}
+
+
+//return a value when checking if post has thumbnail. Use -1 which will not match any existing post_id
+add_filter( "get_post_metadata" , "return_false_thumbnail_id",10,4);
+function return_false_thumbnail_id($value, $object_id, $meta_key, $single ){
+	if(!$single || $meta_key != '_thumbnail_id')
+		return $value;
+		
+	$src = get_post_meta($object_id,'remote_thumbnail',true);
+	if(!$src)
+		return $value;
+	
+	return -1;
+}
